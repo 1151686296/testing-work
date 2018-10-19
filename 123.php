a98c7dfa5e87f4e92a5390af52e8d2f668a64d09
james <1151686296@qq.com>
2018-10-19 20:06:45 +0800

    123.php

--- 123.php
+++ 123.php
@@ -1,12 +1,3 @@
-﻿<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>123</title>
-</head>
-<body>
-<?php
-echo "<P>许秉灏最帅</P>";
-?>
-</body>
-</html>
+﻿<?php
+$num = 12.8;
+printf("%%%s",$num);
