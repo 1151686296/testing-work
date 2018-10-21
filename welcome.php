16585328e03adf6f7455f4f1380da1318218b013
james <1151686296@qq.com>
2018-10-22 00:08:44 +0800

    all

--- welcome.php
+++ welcome.php
@@ -1,3 +1,6 @@
-欢迎<?php echo  $_GET("fname"); ?>!<br>
-你的年龄是<?php echo  $_GET("age"); ?>
+欢迎<?php echo $_GET("fname"); ?>!<br>
+你的年龄是<?php echo $_GET("age"); ?>
+岁
+欢迎<?php echo $_GET("fname"); ?>!<br>
+你的年龄是<?php echo $_GET("age"); ?>
 岁
