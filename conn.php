7ed26d4d60339b0c006fb0494ac4808cad6c7874
james <1151686296@qq.com>
2018-10-21 14:54:01 +0800

    conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 //建立数据库连接；
-$conn=mysqli_connect("localhost","root","*5D6B99008579965AB08847B3A4382DE0C656DA4E.","user");
+$conn=mysqli_connect("izwz9fwt73db2qxedx219pz","root","*5D6B99008579965AB08847B3A4382DE0C656DA4E.","user");
 //设置字符为utf-8，@抑制字符变量的声明提醒。
 @ mysqli_set_charset ($conn,utf8);
 @ mysqli_query($conn,utf8);
