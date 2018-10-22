802b788f641087dab4605b70882058c650c29620
james <1151686296@qq.com>
2018-10-22 22:35:30 +0800

    all

--- welcome.php
+++ welcome.php
@@ -6,6 +6,18 @@ function cs()
 {
     $num=$_GET["fname"];
     echo $num;
+    if($num="许秉灏")
+    {
+        echo "这是个帅哥";
+    }
+    else if ($num="张浩楠")
+    {
+        echo "这是个丑逼";
+    }
+    else
+    {
+        echo "还行吧";
+    }
 }
 cs();
 ?>
