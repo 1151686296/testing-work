ebcb23232293b430482cef20ad86b5c46a90d9ab
james <1151686296@qq.com>
2018-10-22 22:44:23 +0800

    all

--- welcome.php
+++ welcome.php
@@ -2,13 +2,21 @@
 你的年龄是 <?php echo $_GET["age"]; ?>
 岁。
 <?php
+
 function cs()
 {
     $num=$_GET["fname"];
+    $xbh="许秉灏";
+        $zhn="张浩楠";
+
     echo $num;
-    if($num="许秉灏")
+    if($num=$xbh)
     {
         echo "这是个帅哥";
+    }
+    if($num=$zhn);
+    {
+        echo "丑逼";
     }
     else
     {
