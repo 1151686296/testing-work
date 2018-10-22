084c79a5f9ab9eeee32559c4f68112d587c82109
james <1151686296@qq.com>
2018-10-22 22:06:23 +0800

    all

--- welcome.php
+++ welcome.php
@@ -1,3 +1,16 @@
 欢迎 <?php echo $_GET["fname"]; ?>!<br>
 你的年龄是 <?php echo $_GET["age"]; ?>
 岁。
+<?php
+if ($_GET["fname"]=许秉灏)
+{
+    echo 他是个帅哥;
+}
+if ($_GET["fname"]=张浩楠)
+{
+    echo 这是个丑逼;
+}
+else
+{
+    echo 还可以吧;
+}
