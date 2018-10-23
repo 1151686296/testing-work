f2db57fddd4c3438d5005c804cbbefa9d689cd40
james <1151686296@qq.com>
2018-10-23 23:17:38 +0800

    all

--- welcome.php
+++ welcome.php
@@ -6,10 +6,10 @@
 <body>
 <form method="post">
     <input type="text" name="age">
-    <input type="submit" value="提交">
+    <input type="submit" value="提交" name="ok">
 </form>
 <?php
-if(!isset($_POST["submit"]))
+if(!isset($_POST["ok"]))
 {
     echo $_POST["age"];
 }
