50bcf27dc2951a092a76cd401ac720babf2fc872
james <1151686296@qq.com>
2018-10-25 22:50:51 +0800

    all

---
+++ 测试/反馈数据库.php
@@ -0,0 +1,7 @@
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<?php
+$text=$_POST["1"];
+include ("conn.php");
+mysqli_query($conn,"update 修改内容 set 内容=$text where id=1");
+echo "<script>alert('修改成功');history.back();</script>";
+?>
