7e72e24827a518fb2588b8e1b9fd0ae33d917b10
james <1151686296@qq.com>
2018-10-25 20:01:50 +0800

    all

--- 测试/1.php
+++ 测试/1.php
@@ -1,2 +1,10 @@
 <?php
-echo $_POST["text"];
+require_once ('conn.php');
+?>
+<?php
+$sql=mysqli_query($conn,"select * from 修改内容 where id=1");
+$info=mysqli_fetch_array($sql);
+?>
+<?php
+echo $info["内容"];
+?>
