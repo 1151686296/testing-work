838650a67885a5b8535d885a326fd9c232193723
james <1151686296@qq.com>
2018-10-25 14:29:06 +0800

    all

---
+++ 登陆页面/chkuser.php
@@ -0,0 +1,42 @@
+
+<?php
+include("conn.php");
+$username=$_POST['username'];
+$userpwd=$_POST['password'];
+class chkinput{
+   var $name;
+   var $pwd;
+
+   function chkinput($x,$y){
+     $this->name=$x;
+     $this->pwd=$y;
+    }
+
+   function checkinput(){
+     include("conn.php");
+     $sql=mysqli_query($conn,"select * from member where username='".$this->name."'");
+     $info=mysqli_fetch_array($sql);
+     if($info==false){
+          echo "<script language='javascript'>alert('账户输入错误！');history.back();</script>";
+          exit;
+       }
+      else{   if($info['password']==$this->pwd)
+	       {
+			   session_start();
+	           $_SESSION['username']=$info['username'];
+               $_SESSION['ID']=$info['ID'];
+               header("location:admin.php");
+               exit;
+            }
+             else {
+			   echo "<script language='javascript'>alert('密码输入错误！');history.back();</script>";
+             exit;
+           }
+			}
+
+      }    
+   }
+
+
+    $obj=new chkinput(trim($username),trim($userpwd));
+    $obj->checkinput();
