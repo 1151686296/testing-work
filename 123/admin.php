2a9363ec808f589fe3da1ae9e568df1ee3d777fa
james <1151686296@qq.com>
2018-10-26 19:58:57 +0800

    all

--- 123/admin.php
+++ 123/admin.php
@@ -2,5 +2,12 @@
     <?php
    @ session_start();
    echo $_SESSION['inter'];
-
-
+   $inter=$_SESSION['inter'];
+   if($inter<>2)
+   {
+       header("location:index.html");
+   }
+else
+{
+    echo "welcome";
+}
