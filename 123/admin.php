40f55acf4d92f1fddcd35460d5821ed54240d926
james <1151686296@qq.com>
2018-10-26 19:42:15 +0800

    all

--- 123/admin.php
+++ 123/admin.php
@@ -3,4 +3,9 @@
     session_start();
     var_dump($_SESSION['inter']);
     $inter=$_SESSION['inter'];
-    echo $inter;
+    if ($inter<>2)
+    {
+        header("location:index.html");
+
+    }
+    echo "xbhzs";
