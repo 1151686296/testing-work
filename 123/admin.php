cd9079579164489040c016becfef09f502d0f9c2
james <1151686296@qq.com>
2018-10-26 19:22:31 +0800

    all

---
+++ 123/admin.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+var_dump($_SESSION['inter']);
+$inter=$_SESSION['inter'];
+if($inter<>1)
+{
+    header("location:index.html");
+}
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>123</title>
+</head>
+<form method="post" action="反馈数据库.php">
+
+<?php
+require_once ('conn.php');
+?>
+<?php
+$sql=mysqli_query($conn,"select * from 修改内容 where id=1");
+$info=mysqli_fetch_array($sql);
+?>
+<input type="text" size="100" name="1" value="<?php echo $info["内容"]; ?>">
+    <input type="submit" value="提交">
+</form>
+</html>
---
+++ 123/chkuser.php
@@ -0,0 +1,43 @@
+<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+<?php
+include("conn.php");
+$username=$_POST['username'];
+$userpwd=$_POST['password'];
+class chkinput{
+   var $name;
+   var $pwd;
+
+   function chkinput($x,$y){
+     $this->name=$x;
+     $this->pwd=$y;
+    }
+
+   function checkinput(){
+     include("conn.php");
+       $sql=mysqli_query($conn,"select * from 账号密码 where username='".$this->name."'" );
+     $info=mysqli_fetch_array($sql);
+     if($info==false){
+          echo "<script language='javascript'>alert('账户输入错误！');history.back();</script>";
+          exit;
+       }
+      else{   if($info['password']==$this->pwd)
+	       {
+			   session_start();
+	           $_SESSION['username']=$info['username'];
+               header("location:admin.php");
+               exit;
+               session_start();
+               $_SESSION['inter']=1;
+            }
+             else {
+			   echo "<script language='javascript'>alert('密码输入错误！');history.back();</script>";
+             exit;
+           }
+			}
+
+      }    
+   }
+
+
+    $obj=new chkinput(trim($username),trim($userpwd));
+    $obj->checkinput();
