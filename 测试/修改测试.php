7637a06b283637c7c481ff75ffe19ce5476abbcd
james <1151686296@qq.com>
2018-10-26 16:59:33 +0800

    all

--- 测试/修改测试.php
+++ 测试/修改测试.php
@@ -4,6 +4,13 @@
     <title>123</title>
 </head>
 <form method="post" action="反馈数据库.php">
+    <?php
+    $fromurl="http://119.23.18.102/%E7%99%BB%E9%99%86%E9%A1%B5%E9%9D%A2/"; //跳转往这个地址。
+    if( $_SERVER['HTTP_REFERER'] == "http://119.23.18.102/%E6%B5%8B%E8%AF%95/%E4%BF%AE%E6%94%B9%E6%B5%8B%E8%AF%95.php" )
+    {
+    header("Location:".$fromurl); exit;
+    }
+    ?>
 <?php
 require_once ('conn.php');
 ?>
