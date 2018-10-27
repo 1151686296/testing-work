1a5386b67e8ab6eaee450853ca246ff1c2595960
james <1151686296@qq.com>
2018-10-27 19:53:21 +0800

    all

--- index.php
+++ index.php
@@ -580,7 +580,8 @@ $sql=mysqli_query($conn,"select * from 浏览次数 where 网站='精宏首页'"
 $info=mysqli_fetch_array($sql);
 $number=$info["次数"];
 $number=$number +1;
-echo $number;
+mysqli_query($conn,"update 浏览次数 set 次数='$number'where 网站='精宏首页'");
+
 ?>
 
     </body>
