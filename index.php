0bfbc9ada03e15f93e304365fb3357dbb9c52bd0
james <1151686296@qq.com>
2018-10-27 20:54:21 +0800

    all

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-   
-    
- 
+
+
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <title>精弘网络</title>
         <meta charset="UTF-8">
         <style type="text/css">
